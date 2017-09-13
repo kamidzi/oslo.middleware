c4c2e61d7ccbf3ec0db040262964fd322bcdf1b1
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2017-09-13 18:31:39 -0400

    Add entrypoints; non-official.

--- main.py
+++ main.py
@@ -11,71 +11,72 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
-import argparse
-
 from six.moves import SimpleHTTPServer  # noqa
 from six.moves import socketserver
 import webob
 
 from oslo_middleware import healthcheck
+from oslo_config import cfg
 
+import logging
+import sys
+import pdb
 
-class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        @webob.dec.wsgify
-        def dummy_application(req):
-            return 'test'
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
 
+def _create_handler(conf):
+    class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        _conf = conf
 
-        conf = {'backends': 'LdapBind', 'detailed': True}
-#        if req.params.get('detailed', 'false').lower() == 'true':
-#            conf['detailed'] = True
+        def do_GET(self):
+            @webob.dec.wsgify
+            def dummy_application(req):
+                return 'test'
 
-        app = healthcheck.Healthcheck(dummy_application, conf)
-#        if req.accept == 'application/json':
-        req = webob.Request.blank("/healthcheck", accept='application/json',
-                                  method='GET')
-        res = req.get_response(app)
-#        else:
-#            req = webob.Request.blank("/healthcheck", accept='text/html',
-#                                  method='GET')
-#            res = req.get_response(app) 
-        self.send_response(res.status_code)
-        for header_name, header_value in res.headerlist:
-            self.send_header(header_name, header_value)
-        self.end_headers()
-        self.wfile.write(res.body)
-        self.wfile.close()
+            app = healthcheck.Healthcheck(dummy_application, conf)
+            req = webob.Request.blank("/healthcheck", accept='application/json',
+                                    method='GET')
+            res = req.get_response(app)
+            self.send_response(res.status_code)
+            for header_name, header_value in res.headerlist:
+                self.send_header(header_name, header_value)
+            self.end_headers()
+            self.wfile.write(res.body)
+            self.wfile.close()
 
+    return HttpHandler
 
-def positive_int(blob):
-    value = int(blob)
-    if value < 0:
-        msg = "%r is not a positive integer" % blob
-        raise argparse.ArgumentTypeError(msg)
-    return value
-
-
-def create_server(port=0):
-    handler = HttpHandler
-    server = socketserver.TCPServer(("", port), handler)
+def create_server(conf):
+    handler = _create_handler(conf)
+    handler._conf.log_opt_values(lvl=logging.DEBUG, logger=LOG)
+    server = socketserver.TCPServer(("", conf.port), handler)
     return server
 
 
+def _configure_opts():
+    conf = cfg.ConfigOpts()
+    default_opts = [
+            cfg.IntOpt('port',
+                       default=8000,
+                       help='Port to listen on.')
+    ]
+
+    conf.register_opts(default_opts)
+    conf.register_cli_opts(default_opts)
+    conf.register_opts(healthcheck.opts.HEALTHCHECK_OPTS, group='healthcheck')
+    return conf
+
 def main(args=None):
     """Runs a basic http server to show healthcheck functionality."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port",
-                        help="Unused port to run the tiny"
-                             " http server on (or zero to select a"
-                             " random unused port)",
-                        type=positive_int, required=True)
-    args = parser.parse_args(args=args)
-    server = create_server(args.port)
+    conf = _configure_opts()
+    conf(args)
+
+    server = create_server(conf)
     print("Serving at port: %s" % server.server_address[1])
     server.serve_forever()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
+
